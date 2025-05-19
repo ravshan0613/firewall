76853aac9492c0dfa45fdf5eae076786c88f4d4e
Ravshanbek Obidjonov <ravshanbekobidjonov0@gmail.com>
2025-05-19 05:22:39 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+class FirewallExample
+{
+    static void Main()
+    {
+        string ipAddress = "127.0.0.1"; // Tarmoq manzili
+        int port = 80; // Port
+
+        try
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Parse(ipAddress), port);
+            tcpListener.Start();
+            Console.WriteLine("Firewall is running and monitoring the network...");
+            while (true)
+            {
+                TcpClient client = tcpListener.AcceptTcpClient();
+                Console.WriteLine($"Connection attempt detected from {client.Client.RemoteEndPoint}");
+                client.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
